d67c6cb0e327511b1d44bef0fac2579d6384b9bb
Angela <angelamc999@outlook.com>
2019-11-22 12:07:23 +0800

    more google maps api work - got something but still in progress

---
+++ src/sections/Marker.js
@@ -0,0 +1,10 @@
+import { Marker } from "react-google-maps";
+
+{/*Marker*/}
+      <Marker google={this.props.google}
+       name={'Dolores park'}
+          draggable={true}
+          onDragEnd={ this.onMarkerDragEnd }
+             position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
+      />
+      <Marker />
---
+++ src/sections/Usemap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Map from './Map';
+
+
+export function Usemap(props){
+    return (
+
+
+<div style={{ margin: '100px' }}>
+          <Map
+          
+          />
+           </div>
+    
+
+    )
+}
